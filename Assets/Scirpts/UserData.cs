764f8d7873fd636f7f9a56c7171cf78f9920a734
charliebrown1994 <leesungyul@outlook.com>
2025-08-28 11:31:47 +0900

    [Feat] 이승율 : 잔액부족 페널 추가

--- Assets/Scirpts/UserData.cs
+++ Assets/Scirpts/UserData.cs
@@ -6,5 +6,19 @@ using UnityEngine;
 
 public class UserData
 {
-    
+    [Header("UserInfo")]
+    [SerializeField] public string userID;
+    [SerializeField] public string userName;
+    [SerializeField] public string userPW;
+    [SerializeField] public int balance;
+    [SerializeField] public int cash;
+
+    public UserData(string targetID, string targetName, string targetPW, int targetBalance, int targetCash)
+    {
+        userID = targetID;
+        userName = targetName;
+        userPW = targetPW;
+        balance = targetBalance;
+        cash = targetCash;
+    }
 }
